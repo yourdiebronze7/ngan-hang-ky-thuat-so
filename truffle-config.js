2f36010b1a8a179bd153b13473e45d1e33c2eda9
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-08-14 21:44:10 -0700

    Add comments explaining network configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
+// Cấu hình truffle cho mạng phát triển và biên dịch
 module.exports = {
   networks: {
+    // Thông tin cấu hình mạng phát triển
     development: {
       host: "127.0.0.1",
       port: 7545,
@@ -8,6 +10,7 @@ module.exports = {
   },
   compilers: {
     solc: {
+      // Phiên bản trình biên dịch solidity
       version: "^0.8.0"
     }
   }
