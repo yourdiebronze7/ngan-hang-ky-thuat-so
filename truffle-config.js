75e29abd041829653a0e896ae4b92c5989105ad1
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-08-14 22:02:05 -0700

    Add comments for clarity and maintainability

--- truffle-config.js
+++ truffle-config.js
@@ -3,9 +3,9 @@ module.exports = {
   networks: {
     // Thông tin cấu hình mạng phát triển
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+      host: "127.0.0.1", // Địa chỉ máy chủ phát triển
+      port: 7545, // Cổng mạng phát triển
+      network_id: "*" // Bất kỳ network ID nào đều được chấp nhận
     }
   },
   compilers: {
