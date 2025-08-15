620d45829b168b8c7b75cd3a9db7d8b730972180
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-08-14 21:41:09 -0700

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ngân Hàng Kỹ Thuật Số</h1>
+        <p>Chào mừng bạn đến với ứng dụng ví điện tử!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
